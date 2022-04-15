af7a64d7782a7ef329fd6e208285ddb69503c230
James Lucktaylor <jlucktay+github@gmail.com>
2022-04-15 03:12:06 +0100

    fix(fixtures/aws-ec2-gateway): add validation to input variable

--- test/fixtures/aws-ec2-gateway/variables.tf
+++ test/fixtures/aws-ec2-gateway/variables.tf
@@ -1,4 +1,9 @@
 variable "public_key_file" {
   description = "path to a '.pub' file that will be created as a key pair for use with EC2 VMs"
   type        = string
+
+  validation {
+    condition     = length(var.public_key_file) > 4 && substr(var.public_key_file, -4, 4) == ".pub"
+    error_message = "The value for 'public_key_file' must be a path to a '.pub' file."
+  }
 }
